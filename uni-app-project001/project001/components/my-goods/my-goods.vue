1554eb3bcf1fb98909ba8d6a01c6fe673e56e1c7
AugustLYH1998 <60884610+AugustLYH1998@users.noreply.github.com>
2022-09-22 23:27:34 +0800

    完成了商品列表的开发

---
+++ uni-app-project001/project001/components/my-goods/my-goods.vue
@@ -0,0 +1,78 @@
+<template>
+  <view class="list">
+    <!-- 商品图片区 -->
+    <view class="left">
+      <image :src="item.goods_small_logo || defaultPic" class="goods-pic"></image>
+    </view>
+    <!-- 商品信息区 -->
+    <view class="right">
+      <view class="goods-name">
+        {{item.goods_name}}
+      </view>
+      <view class="goods-info-box">
+        <view class="goods-price">
+          ￥{{item.goods_price | tofix}}
+        </view>
+      </view>
+    </view>
+  </view>
+
+</template>
+
+<script>
+  export default {
+    props: {
+      item: {
+        type: Object,
+        default: {}
+      }
+    },
+    name: "my-goods",
+    filters: {
+      tofix(num) {
+        return Number(num).toFixed(2);
+      }
+    },
+    data() {
+      return {
+        // 默认图片地址
+        defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png'
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+  .list {
+    display: flex;
+    padding: 10px 5px;
+    border-bottom: 1px solid #f0f0f0;
+
+    .left {
+      margin-right: 5px;
+
+      .goods-pic {
+        width: 100px;
+        height: 100px;
+        display: block;
+      }
+    }
+
+    .right {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+
+      .goods-name {
+        font-size: 13px;
+      }
+
+      .goods-info-box {
+        .goods-price {
+          font-size: 16px;
+          color: #C00000;
+        }
+      }
+    }
+  }
+</style>
--- uni-app-project001/project001/subpkg/goods_list/goods_list.vue
+++ uni-app-project001/project001/subpkg/goods_list/goods_list.vue
@@ -1,16 +1,94 @@
 <template>
   <view>
-    goods_list
+    <view class="goods_list">
+      <!-- 商品每一项 -->
+      <view v-for="(item,index) in goodsList" :key="index" @click="gotoDeatil(item)">
+        <my-goods :item="item"></my-goods>
+      </view>
+    </view>
+
   </view>
+
 </template>
 
 <script>
   export default {
     data() {
       return {
-        
+        // 请求参数对象
+        queryObj: {
+          query: '',
+          cid: '',
+          pagenum: 1,
+          pagesize: 10
+        },
+        // 商品列表数据
+        goodsList: [],
+        // 总数量
+        total: 0,
+        // 节流阀
+        isloading: false
+
       };
+    },
+
+    onLoad(options) {
+      // console.log(options);
+      // 定义请求参数
+      this.queryObj.query = options.query || ''
+      this.queryObj.cid = options.cid || ''
+      // console.log(this.queryObj);
+
+      // 请求商品列表数据
+      this.getGoodsList();
+    },
+    methods: {
+      async getGoodsList(cb) {
+        this.isloading = true;
+        const {
+          data: res
+        } = await uni.$http.get('/api/public/v1/goods/search?query=' + this.queryObj.query + '&cid=' + this.queryObj
+          .cid +
+          '&pagenum=' + this.queryObj.pagenum + '&pagesize=' + this.queryObj.pagesize);
+        // 关闭节流阀
+        this.isloading = false;
+        // 只要数据请求完毕 立即调用cb回调函数
+        cb && cb();
+        if (res.meta.status !== 200) return uni.$showMsg();
+        this.goodsList = [...this.goodsList, ...res.message.goods];
+        this.total = res.message.total;
+      },
+
+      // 跳转商品详情页
+      gotoDeatil(item) {
+        uni.navigateTo({
+          url: '/subpkg/goods_detail/goods_detail?goods_id=' + item.goods_id
+        })
+      }
+
+    },
+    // 触底让页面+1
+    onReachBottom() {
+      if (this.queryObj.pagenum * this.queryObj.pagesize >= this.total) {
+        return uni.$showMsg('数据加载完毕');
+      }
+
+
+      if (this.isloading) return;
+      this.queryObj.pagenum++;
+      this.getGoodsList()
+    },
+    onPullDownRefresh() {
+      this.queryObj.pagenum = 1;
+      this.total = 0;
+      this.isloading = false;
+      this.goodsList = [];
+
+      this.getGoodsList(() => {
+        uni.stopPullDownRefresh()
+      })
     }
+
   }
 </script>
 
