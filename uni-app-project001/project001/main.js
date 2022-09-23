5ca48d80360232019140f7c32c371d57eb59ccf3
AugustLYH1998 <60884610+AugustLYH1998@users.noreply.github.com>
2022-09-24 03:56:05 +0800

    完成了登录和支付功能的开发

--- uni-app-project001/project001/main.js
+++ uni-app-project001/project001/main.js
@@ -16,10 +16,19 @@ $http.baseUrl = 'https://api-hmugo-web.itheima.net';
 
 // 请求拦截器
 // // 请求开始之前做一些事情
-$http.beforeRequest = function(option) {
+$http.beforeRequest = function(options) {
   uni.showLoading({
     title: '数据加载中...'
   })
+
+  // 判断请求的是否为有权限的 API 接口
+  if (options.url.indexOf('/my/') !== -1) {
+    // 为请求头添加身份认证字段
+    options.header = {
+      // 字段的值可以直接从 vuex 中进行获取
+      Authorization: store.state.m_user.token,
+    }
+  }
 }
 
 // 响应拦截器
@@ -31,6 +40,13 @@ $http.afterRequest = function() {
 
 // 封装弹框方法
 uni.$showMsg = function(title = '数据请求失败', duration = 1500) {
+  uni.showToast({
+    title,
+    duration,
+    icon: 'none'
+  })
+}
+uni.$showError = function(title = '数据请求失败', duration = 1500) {
   uni.showToast({
     title,
     duration,
