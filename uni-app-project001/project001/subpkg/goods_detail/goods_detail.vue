734ff73dec30e353c40d8608aab3e75cca81ea96
AugustLYH1998 <60884610+AugustLYH1998@users.noreply.github.com>
2022-09-23 11:08:13 +0800

    完成了商品详情页的开发

--- uni-app-project001/project001/subpkg/goods_detail/goods_detail.vue
+++ uni-app-project001/project001/subpkg/goods_detail/goods_detail.vue
@@ -1,6 +1,48 @@
 <template>
-  <view>
-    
+  <view v-if="goods_info.goods_name" class="goods-box">
+    <!-- 轮播图区域 -->
+    <swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+      <swiper-item v-for="(item,index) in goods_info.pics" :key="index">
+        <image :src="item.pics_big" @click="preview(index)"></image>
+      </swiper-item>
+    </swiper>
+
+    <!-- 商品信息区 -->
+    <view class="goods-info">
+      <!-- 商品价格 -->
+      <view class="goods-info-price">
+        ￥{{goods_info.goods_price}}
+      </view>
+      <!-- 商品信息主体区 -->
+      <view class="goods-info-body">
+        <!-- 商品名字 -->
+        <view class="goods-name">
+          {{goods_info.goods_name}}
+        </view>
+        <!-- 收藏按钮 -->
+        <view class="goods-favior">
+          <uni-icons type="star" size="15" color="gray"></uni-icons>
+          <text>收藏</text>
+
+        </view>
+      </view>
+      <!-- 运费 -->
+      <view class="goods-info-yf">
+        快递：免运费
+      </view>
+
+      <!-- 富文本内容 -->
+      <rich-text :nodes="goods_info.goods_introduce"></rich-text>
+
+
+    </view>
+
+    <!-- 商品导航按钮 -->
+    <view class="goods_nav">
+      <uni-goods-nav :fill="true" :options="options" :buttonGroup="buttonGroup" @click="onClick"
+        @buttonClick="buttonClick" />
+    </view>
+
   </view>
 </template>
 
@@ -8,12 +50,116 @@
   export default {
     data() {
       return {
-        
+        goods_info: {},
+        options: [{
+          icon: 'shop',
+          text: '店铺',
+        }, {
+          icon: 'cart',
+          text: '购物车',
+          info: 9
+        }],
+        buttonGroup: [{
+            text: '加入购物车',
+            backgroundColor: '#ff0000',
+            color: '#fff'
+          },
+          {
+            text: '立即购买',
+            backgroundColor: '#ffa200',
+            color: '#fff'
+          }
+        ]
       };
+    },
+    methods: {
+      async getGoodsDetail(goods_id) {
+        const {
+          data: res
+        } = await uni.$http.get('/api/public/v1/goods/detail?goods_id=' + goods_id);
+        if (res.meta.status !== 200) return $uni.$showMsg();
+
+        // 对详情图片进行空行处理 图片后缀处理
+        res.message.goods_introduce = res.message.goods_introduce.replace(/<img /g, '<img style="display:block;" ')
+          .replace(/webp/g, 'jpg');
+
+        this.goods_info = res.message;
+      },
+      preview(index) {
+        uni.previewImage({
+          current: index,
+          urls: this.goods_info.pics.map(x => x.pics_big)
+        })
+      },
+      onClick(e) {
+        console.log(e);
+        if (e.content.text === '购物车') {
+          uni.switchTab({
+            url: '/pages/cart/cart'
+          })
+        }
+      }
+    },
+    onLoad(options) {
+      const goods_id = options.goods_id;
+      this.getGoodsDetail(goods_id);
     }
   }
 </script>
 
 <style lang="scss">
+  .goods-box {
+    padding-bottom: 50px;
+  }
 
+  swiper {
+    height: 750rpx;
+
+    image {
+      width: 100%;
+      height: 100%;
+    }
+  }
+
+  .goods-info {
+    padding: 10px 10px;
+  }
+
+  .goods-info-price {
+    font-size: 18px;
+    color: #C00000;
+    margin: 10px 0px;
+  }
+
+  .goods-info-body {
+    display: flex
+  }
+
+  .goods-name {
+    font-size: 13px;
+    margin-right: 10px;
+  }
+
+  .goods-favior {
+    width: 120px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-size: 13px;
+    border-left: 1px solid #efefef;
+    color: gray;
+  }
+
+  .goods-info-yf {
+    font-size: 10px;
+    color: gray;
+    margin: 10px 0;
+  }
+
+  .goods_nav {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+  }
 </style>
